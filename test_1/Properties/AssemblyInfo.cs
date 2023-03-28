7e3e5aa70a3f268215c0bddb148ae6bc26fdc287
Nicky Edge <nicky.edge@sky.uk>
2023-03-28 16:05:03 +0100

    testaaaaaaaaaaaaaaah

---
+++ test_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("test_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2727043B-FBBC-4A29-B3C7-402ADBF0B7AF")]
+[assembly: AssemblyVersion("1.0.0.0")]
