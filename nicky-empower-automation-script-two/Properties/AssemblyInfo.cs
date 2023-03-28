beeca347c98da4ea4a273d6db9841f04eaa294c1
Nicky Edge <nicky.edge@sky.uk>
2023-03-28 15:46:59 +0100

    add another script

---
+++ nicky-empower-automation-script-two/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("nicky-empower-automation-script-two")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("nicky-empower-automation-script-two")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7CEEF23E-F2A4-4CE0-A688-8D165A498F12")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ nicky-empower-automation-script-two_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("nicky-empower-automation-script-two_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("nicky-empower-automation-script-two_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4FE6CABE-44D1-41EB-A95D-5BACD03AFD67")]
+[assembly: AssemblyVersion("1.0.0.0")]
